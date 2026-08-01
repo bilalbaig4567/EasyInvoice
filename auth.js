b4700b619125022da37fdd0714f7867ee99a1dc1
bilalbaig4567 <bbaig7965@gmail.com>
2026-08-02 01:20:06 +0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,46 @@
-import { app } from "./firebase.js";
+import {
+  auth,
+  provider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "./firebase.js";
 
-console.log("Firebase Connected Successfully!", app);
+window.signUp = async function () {
+  let email = document.getElementById("loginEmail").value;
+  let password = document.getElementById("loginPassword").value;
+  let confirm = document.getElementById("confirmPassword").value;
+
+  if (email === "" || password === "") {
+    alert("Please fill all fields");
+    return;
+  }
+
+  if (password !== confirm) {
+    alert("Passwords do not match");
+    return;
+  }
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    alert("✅ Account Created Successfully!");
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+window.loginUser = async function () {
+  let email = document.getElementById("loginEmail").value;
+  let password = document.getElementById("loginPassword").value;
+
+  if (email === "" || password === "") {
+    alert("Please enter email and password");
+    return;
+  }
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("✅ Login Successful!");
+  } catch (error) {
+    alert(error.message);
+  }
+};

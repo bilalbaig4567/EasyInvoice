426364becc52b09c780cf91597bbf4460eaa95d3
bilalbaig4567 <bbaig7965@gmail.com>
2026-08-03 01:20:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,74 @@
+// =======================================
+// EasyInvoice Pro Max V9
+// app.js - Part 1
+// =======================================
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    initTheme();
+
+    initNavigation();
+
+});
+
+// ==========================
+// Dark Mode
+// ==========================
+
+function initTheme(){
+
+const btn=document.getElementById("themeBtn");
+
+btn.addEventListener("click",()=>{
+
+document.body.classList.toggle("dark");
+
+if(document.body.classList.contains("dark")){
+
+localStorage.setItem("theme","dark");
+
+btn.innerHTML='<i class="fa-solid fa-sun"></i>';
+
+}else{
+
+localStorage.setItem("theme","light");
+
+btn.innerHTML='<i class="fa-solid fa-moon"></i>';
+
+}
+
+});
+
+if(localStorage.getItem("theme")==="dark"){
+
+document.body.classList.add("dark");
+
+btn.innerHTML='<i class="fa-solid fa-sun"></i>';
+
+}
+
+}
+
+// ==========================
+// Navigation
+// ==========================
+
+function initNavigation(){
+
+const items=document.querySelectorAll(".sidebar li");
+
+items.forEach(item=>{
+
+item.addEventListener("click",()=>{
+
+items.forEach(i=>i.classList.remove("active"));
+
+item.classList.add("active");
+
+});
+
+});
+
+}
+
+console.log("✅ EasyInvoice Pro Max Loaded");
